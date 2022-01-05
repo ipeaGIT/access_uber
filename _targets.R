e370088896262840b8c4e6ce40144a1407e5dad1
Daniel Herszenhut <dhersz@gmail.com>
2022-01-05 12:02:16 -0300

    added rapid transit stations target

--- _targets.R
+++ _targets.R
@@ -1,11 +1,23 @@
+options(
+  java.parameters = "-Xmx100G",
+  R5R_THREADS = 15
+)
+
 suppressPackageStartupMessages({
   library(targets)
+  library(data.table)
+  library(r5r)
 })
 
 list(
   tar_target(
     uber_data,
     "../../data-raw/uber_speed/orig_ds_anonymized.rds",
+    format = "file"
+  ),
+  tar_target(
+    rapid_transit_stations,
+    "../../data-raw/uber_speed/rapid_transit_stations_city.csv",
     format = "file"
   )
 )
